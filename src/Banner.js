0f7316f0104922676eaa77dbf5840b38e7f4c276
Riya <vriya944@gmail.com>
2023-05-09 21:43:29 +0530

    commit

--- src/Banner.js
+++ src/Banner.js
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+} from "react";
 import "./Banner.css";
 import axios from "./axios";
-import requests  from "./Request"
+import requests from "./Request";
 
 export default function Banner() {
-  const [movie,setMovie]= useState([])
+  const [movie, setMovie] = useState([]);
 
-  useEffect(()=>{
-    async function fetchData(){
-        const request  = await axios.get(requests.fetchNetflixOriginals)
-        console.log("request",request);
-       setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length-1 )])
-       return request
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(
+        requests.fetchNetflixOriginals
+      );
+      setMovie(
+        request.data.results[
+          Math.floor(
+            Math.random() *
+              request.data.results.length -
+              1
+          )
+        ]
+      );
+      return request;
     }
-    fetchData()
-    
-  },[])
+    fetchData();
+  }, []);
 
-
-
-  function truncate(string,n){
-    return string?.length > n ? string.substr(0,n-1) + "..." : string
-
+  function truncate(string, n) {
+    return string?.length > n
+      ? string.substr(0, n - 1) + "..."
+      : string;
   }
-
 
   return (
     <header
@@ -35,14 +44,22 @@ export default function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">{movie?.title}</h1>
+        <h1 className="banner_title">
+          {movie?.title}
+        </h1>
         <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My list</button>
+          <button className="banner_button">
+            Play
+          </button>
+          <button className="banner_button">
+            My list
+          </button>
         </div>
-        <h1 className="banner_description">{truncate(movie?.overview,150)}</h1>
+        <h1 className="banner_description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
-      <div className="banner_fadeBottom"/>
+      <div className="banner_fadeBottom" />
     </header>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,36 +1,50 @@
-import axios from './axios'
-import React, { useEffect, useState } from 'react'
-import "./Row.css"
+import axios from "./axios";
+import React, {
+  useEffect,
+  useState,
+} from "react";
+import "./Row.css";
 
-function Row({title,fetchUrl,isLargeRow = false}) {
-   const [movies,setMovies] = useState([])
+function Row({
+  title,
+  fetchUrl,
+  isLargeRow = false,
+}) {
+  const [movies, setMovies] = useState([]);
 
-   const base_url = "https://api.themoviedb.org/3"
-
-   useEffect(()=>{
-    async function fetchData(){
-    
-         const request = await axios.get(fetchUrl)
-         setMovies(request.data.results) 
-        ;
-         return request
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
-    fetchData()
-   },[fetchUrl])
+    fetchData();
+  }, [fetchUrl]);
 
-   console.log(movies);
+  console.log(movies);
 
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className='row-posters'>
-       {movies.map(movie => {
-        return( <img className={`row-poster ${isLargeRow && "row-posterLarge"}`} src={`https://image.tmdb.org/t/p/original${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>)
-       
-       })}
-       </div>
+      <div className="row-posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              className={`row-poster ${
+                isLargeRow && "row-posterLarge"
+              }`}
+              src={`https://image.tmdb.org/t/p/original${
+                isLargeRow
+                  ? movie.poster_path
+                  : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
